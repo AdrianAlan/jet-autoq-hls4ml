d548522f50425c250ecdbb0839c3f83ed8159c34
AdrianAlan <adrianalan.pol@gmail.com>
2020-07-17 05:19:50 -0400

    Unarchive fetched files

--- prepare-dataset.py
+++ prepare-dataset.py
@@ -1,5 +1,6 @@
 import configparser
 import requests
+import tarfile
 
 
 class PrepareDataset():
@@ -15,13 +16,26 @@ class PrepareDataset():
         test_dataset = self.config.get('source', 'TestDataset')
         tmp_directory = self.config.get('store', 'TmpDirectory')
 
-        self.fetch(train_dataset, '%s/train.tar.gz' % tmp_directory)
-        self.fetch(test_dataset, '%s/test.tar.gz' % tmp_directory)
+        train_path = '%s/train.tar.gz' % tmp_directory
+        test_path = '%s/test.tar.gz' % tmp_directory
+
+        # Fetch files from url
+        self.fetch(train_dataset, train_path)
+        self.fetch(test_dataset, test_path)
+
+        # Unarchive fetched files
+        self.unarchive(train_path, tmp_directory)
+        self.unarchive(test_path, tmp_directory)
 
     def fetch(self, url, store):
         r = requests.get(url)
         with open(store, 'wb') as f:
             f.write(r.content)
+
+    def unarchive(self, fname, tmpdir):
+        tar = tarfile.open(fname, "r:gz")
+        tar.extractall(path=tmpdir)
+        tar.close()
 
 
 pd = PrepareDataset(r'./config.txt')
