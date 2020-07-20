07231aa787af859801828929bd019d8f6778c663
AdrianAlan <adrianalan.pol@gmail.com>
2020-07-20 08:59:47 -0400

    Export constants to config

--- prepare-dataset.py
+++ prepare-dataset.py
@@ -1,4 +1,3 @@
-import configparser
 import h5py
 import numpy as np
 import os
@@ -6,17 +5,15 @@ import requests
 import shutil
 import tarfile
 
+from configparser import ConfigParser, ExtendedInterpolation
+
 
 class PrepareDataset():
 
     def __init__(self, config_path):
 
         # Load configuration
-        self.config = configparser.RawConfigParser()
-        self.config.read(config_path)
-
-        self.X_data_shape = (100, 100, 1)
-        self.y_data_shape = (5,)
+        self.parse_config(config_path)
 
     def cleanup(self, tmpdir):
         shutil.rmtree(tmpdir)
@@ -26,37 +23,53 @@ class PrepareDataset():
             os.makedirs(directory)
 
     def download_data(self):
-        train_dataset = self.config.get('source', 'TrainDataset')
-        test_dataset = self.config.get('source', 'TestDataset')
-        tmp_dir = self.config.get('store', 'TmpDirectory')
-        save_dir = self.config.get('store', 'SaveDirectory')
-
-        train_path = '%s/train.tar.gz' % tmp_dir
-        test_path = '%s/test.tar.gz' % tmp_dir
 
         # Creating directories
-        self.create_directory(tmp_dir)
-        self.create_directory(save_dir)
+        self.create_directory(self.tmp_dir)
+        self.create_directory(self.save_dir)
 
         # Fetch files from url
-        self.fetch(train_dataset, train_path)
-        self.fetch(test_dataset, test_path)
+        self.fetch(self.train_dataset, self.tmp_train_path)
+        self.fetch(self.test_dataset, self.tmp_test_path)
 
         # Unarchive fetched files
-        self.unarchive(train_path, tmp_directory)
-        self.unarchive(test_path, tmp_directory)
+        self.unarchive(self.tmp_train_path, self.tmp_dir)
+        self.unarchive(self.tmp_test_path, self.tmp_dir)
 
-        self.process_data('%s/train' % tmp_directory,
-                          save_directory, 'train')
+        self.process_data(self.train_dataset_dir, self.save_dir, 'train',
+                          self.X_data_shape, self.y_data_shape)
+        self.process_data(self.test_dataset_dir, self.save_dir, 'test',
+                          self.X_data_shape, self.y_data_shape)
 
-        self.cleanup(tmp_dir)
+        self.cleanup(self.tmp_dir)
 
     def fetch(self, url, store):
         r = requests.get(url)
         with open(store, 'wb') as f:
             f.write(r.content)
 
-    def process_data(self, in_dir, out_dir, set_type):
+    def parse_config(self, config_path):
+        self.config = ConfigParser(interpolation=ExtendedInterpolation(),
+                                   converters={'tuple': self.parse_int_tuple})
+        self.config.read(config_path)
+        self.train_dataset = self.config.get('source', 'TrainDataset')
+        self.train_dataset_dir = self.config.get('source', 'TrainDirectory')
+        self.test_dataset = self.config.get('source', 'TestDataset')
+        self.test_dataset_dir = self.config.get('source', 'TestDirectory')
+
+        self.tmp_dir = self.config.get('store', 'TmpDirectory')
+        self.save_dir = self.config.get('store', 'SaveDirectory')
+        self.tmp_train_path = self.config.get('store', 'TmpTrainPath')
+        self.tmp_test_path = self.config.get('store', 'TmpTestPath')
+
+        self.X_data_shape = self.config.gettuple('data', 'XShape')
+        self.y_data_shape = self.config.gettuple('data', 'yShape')
+
+    def parse_int_tuple(self, input):
+        return tuple(int(k.strip()) for k in input[1:-1].split(','))
+
+    def process_data(self, in_dir, out_dir, set_type,
+                     X_data_shape, y_data_shape):
         X_set = 'X_%s' % set_type
         y_set = 'y_%s' % set_type
 
@@ -71,17 +84,17 @@ class PrepareDataset():
 
                 if row == 0:
                     h5.create_dataset(X_set, dtype=np.dtype('float64'),
-                                      shape=(0,) + self.X_data_shape,
-                                      maxshape=(None,) + self.X_data_shape)
+                                      shape=(0,) + X_data_shape,
+                                      maxshape=(None,) + X_data_shape)
                     h5.create_dataset(y_set, dtype=np.dtype('float64'),
-                                      shape=(0,) + self.y_data_shape,
-                                      maxshape=(None,) + self.y_data_shape)
+                                      shape=(0,) + y_data_shape,
+                                      maxshape=(None,) + y_data_shape)
 
                 crows = y.shape[0]
 
-                h5[X_set].resize(((row+crows,) + self.X_data_shape))
+                h5[X_set].resize(((row+crows,) + X_data_shape))
                 h5[X_set][row:row+crows, :] = X
-                h5[y_set].resize(((row+crows,) + self.y_data_shape))
+                h5[y_set].resize(((row+crows,) + y_data_shape))
                 h5[y_set][row:row+crows, :] = y
 
                 row += crows
